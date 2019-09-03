82c1fa30dc67a4b38f446fb6aa5dd0f2ff58f4e4
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-03 10:51:44 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSeñales/EscalonUnitario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class EscalonUnitario
+    {
+        public List<Muestra> Muestras { get; set; }
+
+        public EscalonUnitario()
+        {
+            Muestras = new List<Muestra>();
+        }
+
+        public double evaluar(double tiempo)
+        {
+            double resultado;
+
+            if (tiempo >= 0)
+            {
+                resultado = 1.0;
+            }else
+            {
+                resultado = 0.0;
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ GraficadorSeñales/SenalParabolica.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class SenalParabolica
+    {
+        public List<Muestra> Muestras { get; set; }
+
+        public SenalParabolica()
+        {
+            Muestras = new List<Muestra>();
+        }
+
+        public double evaluar(double tiempo)
+        {
+            double resultado;
+
+            if (tiempo >= 0)
+            {
+                resultado = (tiempo * tiempo) / 2.0;
+            }else
+            {
+                resultado = 0.0;
+            }
+
+            return resultado;
+        }
+    }
+}
